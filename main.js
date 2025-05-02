03d3a5497d8819ac4a99bb96f540ae2c8f297534
Iago <iago.f.silva@hotmail.com>
2025-05-02 19:51:13 -0300

    agora

--- main.js
+++ main.js
@@ -2,19 +2,26 @@ $(document).ready(function() {
     $('header button').click (function() {
         alert("Expandir formulario")
     })
+    
+    $('ul').on('click', 'li',function (e) {
+        $(this).css('text-decoration', 'line-trough');
+    })
 
     $('form').on('submit', function (e) {
         e.preventDefault();
         const enderecoDaNovaTarefa = $('#tarefa').val();
         const novaTarefa = $('<li></li>');
+        
         $(`
             <div class="link">
-                <a href="${enderecoDaNovaTarefa}" target="_blank" tittle="nova tarefa">
-                    
+                <a target="_blank" tittle="nova tarefa">
+                ${enderecoDaNovaTarefa}
                 </a>
             </div>
         `).appendTo(novaTarefa);
         $(novaTarefa).appendTo('ul');
         $('#tarefa').val('')
+
     })
 })
+
